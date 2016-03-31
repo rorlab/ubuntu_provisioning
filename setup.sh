2666a7bba135396de7f423a7fddde0da201723bb
Lucius Choi <rorlab@gmail.com>
2016-03-31 18:57:01 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-echo "Provision virtual machine..."
+promptyn () {
+    while true; do
+        read -p "$1 " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
 
 echo "Updating Packages"
   sudo apt-get update > /dev/null 2>&1
-  
+
 echo "Installing Korean Language Pack"
   sudo apt-get install language-pack-ko -y > /dev/null 2>&1
 
@@ -20,8 +29,10 @@ echo "Installing Nodejs"
 echo "Installing Imagemagick"
   sudo apt-get install imagemagick -y > /dev/null 2>&1
 
-echo "Installing sqlite3"
-  sudo apt-get install libsqlite3-dev -y > /dev/null 2>&1
+if promptyn "Do you wish to install sqlite3? (y/n)"; then
+    echo "Installing sqlite3"
+    sudo apt-get install libsqlite3-dev -y > /dev/null 2>&1
+fi
 
 echo "Installing MySQL"
   sudo apt-get install debconf-utils -y > /dev/null 2>&1
@@ -29,35 +40,15 @@ echo "Installing MySQL"
   sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 12345678'
   sudo apt-get install mysql-server mysql-client libmysqlclient-dev -y > /dev/null 2>&1
 
-echo "Installing Redis"
-  sudo apt-get install redis -y > /dev/null 2>&1
+if promptyn "Do you wish to install Redis? (y/n)"; then
+    echo "Installing Redis"
+    sudo apt-get install redis -y > /dev/null 2>&1
+fi
 
-echo "Installing Pygments"
-  sudo apt-get install python-pygments > /dev/null 2>&1
+if promptyn "Do you wish to install Pygments? (y/n)"; then
+    echo "Installing Pygments"
+    sudo apt-get install python-pygments > /dev/null 2>&1
+fi
 
 echo "Installing Miscellaneous Libraries"
   sudo apt-get install -y libssl-dev libreadline-dev > /dev/null 2>&1
-
-echo "Installing rbenv"
-  if [ ! -d "$HOME/.rbenv" ]; then
-    sudo -H -u vagrant bash -i -c 'mkdir $HOME/.rbenv' > /dev/null 2>&1
-    git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv > /dev/null 2>&1
-  fi
-  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-  echo 'eval "$(rbenv init -)"'               >> ~/.bashrc
-  source ~/.bashrc
-
-echo "Installing ruby-build"
-  if [ ! -d "$HOME/.rbenv/plugins/ruby-build" ]; then
-    sudo -H -u vagrant bash -i -c 'mkdir -p $HOME/.rbenv/plugins/ruby-build' > /dev/null 2>&1
-    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build > /dev/null 2>&1
-  fi
-
-echo "Installing ruby 2.3.0"
-  sudo -H -u vagrant bash -i -c 'rbenv install 2.3.0' > /dev/null 2>&1
-  sudo -H -u vagrant bash -i -c 'rbenv rehash' > /dev/null 2>&1
-  sudo -H -u vagrant bash -i -c 'rbenv global 2.3.0' > /dev/null 2>&1
-  sudo -H -u vagrant bash -i -c 'gem install bundler --no-ri --no-rdoc' > /dev/null 2>&1
-  sudo -H -u vagrant bash -i -c 'rbenv rehash' > /dev/null 2>&1
-
-echo "Provision completed!"
